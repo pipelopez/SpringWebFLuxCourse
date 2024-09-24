7a15131a7e16564c7f831b2db294ed3b8f0b692e
Pipe Lopez <jflopezv@sura.com.co>
2024-09-24 16:49:44 -0500

    Se adicionan ajustes

--- spring-boot-webflux/src/main/java/com/bolsadeideas/springboot/webflux/app/models/dao/ProductoDao.java
+++ spring-boot-webflux/src/main/java/com/bolsadeideas/springboot/webflux/app/models/dao/ProductoDao.java
@@ -1,5 +1,9 @@
 package com.bolsadeideas.springboot.webflux.app.models.dao;
 
-public class ProductoDao {
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+
+import com.bolsadeideas.springboot.webflux.app.models.documents.Producto;
+
+public interface ProductoDao extends ReactiveMongoRepository<Producto, String>{
 
 }
