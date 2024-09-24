a88fcf7027a9ad7987cf506db2fd883139a0b658
Pipe Lopez <jflopezv@sura.com.co>
2024-09-23 19:30:10 -0500

    Se adiciona manejo de errores

--- spring-boot-reactor/src/main/java/com/bolsadeideas/springboot/reactor/app/SpringBootReactorApplication.java
+++ spring-boot-reactor/src/main/java/com/bolsadeideas/springboot/reactor/app/SpringBootReactorApplication.java
@@ -1,5 +1,7 @@
 package com.bolsadeideas.springboot.reactor.app;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,6 +10,8 @@ import reactor.core.publisher.Flux;
 
 @SpringBootApplication
 public class SpringBootReactorApplication implements CommandLineRunner {
+	
+	private static final Logger log = LoggerFactory.getLogger(SpringBootReactorApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootReactorApplication.class, args);
@@ -15,10 +19,18 @@ public class SpringBootReactorApplication implements CommandLineRunner {
 	
 	@Override
 	public void run(String... args) throws Exception {
-		Flux<String> nombres = Flux.just("Andres", "Jaime", "Diego", "Pedro")
-				.doOnNext(System.out::println);
+		Flux<String> nombres = Flux.just("Andres", "Jaime", "","Diego", "Pedro")
+				.doOnNext(e -> {					
+					if(e.isEmpty()) {
+						throw new RuntimeException("Los elementos no pueden ser vacíos");
+					}
+					
+					System.out.println(e);
+					
+					});
 		
-		nombres.subscribe();
+		nombres.subscribe(e -> log.info(e), 
+				error -> log.error(error.getMessage()));
 	}
 
 }
