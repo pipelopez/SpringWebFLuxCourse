b9a9c4be3de42990fc5cb9c2b0f2f99669e56e16
Pipe Lopez <jflopezv@sura.com.co>
2024-09-24 17:52:40 -0500

    Se adiciona el campo createAt al objeto producto y se crea una nueva base de datos en el application.properties

--- spring-boot-webflux/src/main/java/com/bolsadeideas/springboot/webflux/app/SpringBootWebfluxApplication.java
+++ spring-boot-webflux/src/main/java/com/bolsadeideas/springboot/webflux/app/SpringBootWebfluxApplication.java
@@ -1,5 +1,7 @@
 package com.bolsadeideas.springboot.webflux.app;
 
+import java.util.Date;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -44,7 +46,10 @@ public class SpringBootWebfluxApplication implements CommandLineRunner {
 				new Producto("Mica Cómoda 5 Cajones", 150.89),
 				new Producto("TV Sony Bravia OLED 4K Ultra HD", 2255.89)
 				)
-		.flatMap(producto -> dao.save(producto))
+		.flatMap(producto -> {
+			producto.setCreateAt(new Date());
+			return dao.save(producto);
+			})
 		.subscribe(producto -> log.info("Insert: " + producto.getId() + " " + producto.getNombre()));
 	}
 }
