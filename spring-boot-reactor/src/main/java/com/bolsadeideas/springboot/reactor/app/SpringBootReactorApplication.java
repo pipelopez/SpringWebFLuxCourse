cf978502f63f00163c643ec46b2104cd42672bec
Pipe Lopez <jflopezv@sura.com.co>
2024-09-23 19:19:09 -0500

    Se crea la configuración del proyecto con Java 17 y se crae el primer stream

---
+++ spring-boot-reactor/src/main/java/com/bolsadeideas/springboot/reactor/app/SpringBootReactorApplication.java
@@ -0,0 +1,24 @@
+package com.bolsadeideas.springboot.reactor.app;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import reactor.core.publisher.Flux;
+
+@SpringBootApplication
+public class SpringBootReactorApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootReactorApplication.class, args);
+	}
+	
+	@Override
+	public void run(String... args) throws Exception {
+		Flux<String> nombres = Flux.just("Andres", "Jaime", "Diego", "Pedro")
+				.doOnNext(System.out::println);
+		
+		nombres.subscribe();
+	}
+
+}
