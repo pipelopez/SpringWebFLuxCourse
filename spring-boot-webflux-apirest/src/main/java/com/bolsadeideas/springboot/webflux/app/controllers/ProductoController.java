97e6ee77e21caac6b36c06834b80b1078ef3e7b1
Pipe Lopez <jflopezv@sura.com.co>
2024-09-25 17:32:02 -0500

    Se adiciona el obtener producto por Id

--- spring-boot-webflux-apirest/src/main/java/com/bolsadeideas/springboot/webflux/app/controllers/ProductoController.java
+++ spring-boot-webflux-apirest/src/main/java/com/bolsadeideas/springboot/webflux/app/controllers/ProductoController.java
@@ -1,5 +1,39 @@
 package com.bolsadeideas.springboot.webflux.app.controllers;
 
-public class ProductoController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.bolsadeideas.springboot.webflux.app.models.documents.Producto;
+import com.bolsadeideas.springboot.webflux.app.models.services.ProductoService;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/api/productos")
+public class ProductoController {
+	
+	@Autowired
+	private ProductoService service;
+	
+	@GetMapping
+	public Mono<ResponseEntity<Flux<Producto>>> lista(){
+		return Mono.just(ResponseEntity.ok()
+				.contentType(MediaType.APPLICATION_JSON_UTF8)
+				.body(service.findAll())
+				);
+	}
+	
+	@GetMapping("/{id}")
+	public Mono<ResponseEntity<Producto>> ver(@PathVariable String id){
+		return service.findById(id).map(p -> ResponseEntity.ok()
+				.contentType(MediaType.APPLICATION_JSON_UTF8)
+				.body(p))
+				.defaultIfEmpty(ResponseEntity.notFound().build());
+	}
 }
