0f0d871ed524c56e9d0d5ea75f65b38fe6349286
Pipe Lopez <jflopezv@sura.com.co>
2024-09-25 15:17:44 -0500

    Se adiciona la vista para el detalle de los productos y se les da formato a los campos

--- spring-boot-webflux/src/main/java/com/bolsadeideas/springboot/webflux/app/controller/ProductoController.java
+++ spring-boot-webflux/src/main/java/com/bolsadeideas/springboot/webflux/app/controller/ProductoController.java
@@ -47,6 +47,22 @@ public class ProductoController {
 		return service.findAllCategoria();
 	}
 	
+	@GetMapping("/ver/{id}")
+	public Mono<String> ver(Model model, @PathVariable String id){
+		return service.findById(id)
+				.doOnNext(p -> {
+					model.addAttribute("producto", p);
+					model.addAttribute("titulo", "Detalle Producto");
+				}).switchIfEmpty(Mono.just(new Producto()))
+				.flatMap(p -> {
+					if(p.getId() == null) {
+						return Mono.error(new InterruptedException("No existe el producto"));
+					}
+					return Mono.just(p);
+				}).then(Mono.just("ver"))
+				.onErrorResume(ex -> Mono.just("redirect:/listar?error=no+existe+el+producto"));
+	}
+	
 	@GetMapping({"/listar", "/"})
 	public Mono<String> listar(Model model) {
 		
@@ -79,9 +95,8 @@ public class ProductoController {
 				.flatMap(p -> {
 					if(p.getId() == null) {
 						return Mono.error(new InterruptedException("No existe el producto"));
-					}else {
-						return Mono.just(p);
 					}
+					return Mono.just(p);					
 				})
 				.then(Mono.just("form"))
 				.onErrorResume(ex -> Mono.just("redirect:/listar?error=no+existe+el+producto"));
