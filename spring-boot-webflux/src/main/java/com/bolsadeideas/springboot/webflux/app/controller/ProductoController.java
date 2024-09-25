72b8484fa913d7ab407881e4c2cbe50b31feb642
Pipe Lopez <jflopezv@sura.com.co>
2024-09-25 15:31:11 -0500

    Se adiciona funcionalidad para mostrar la imagen en el detalle del producto

--- spring-boot-webflux/src/main/java/com/bolsadeideas/springboot/webflux/app/controller/ProductoController.java
+++ spring-boot-webflux/src/main/java/com/bolsadeideas/springboot/webflux/app/controller/ProductoController.java
@@ -1,6 +1,9 @@
 package com.bolsadeideas.springboot.webflux.app.controller;
 
 import java.io.File;
+import java.net.MalformedURLException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.time.Duration;
 import java.util.Date;
 import java.util.UUID;
@@ -9,6 +12,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.http.codec.multipart.FilePart;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -45,6 +52,19 @@ public class ProductoController {
 	@ModelAttribute("categorias")
 	public Flux<Categoria> categorias() {
 		return service.findAllCategoria();
+	}
+	
+	@GetMapping("/uploads/img/{nombreFoto:.+}")
+	public Mono<ResponseEntity<Resource>> verFoto(@PathVariable String nombreFoto) throws MalformedURLException{
+		Path ruta = Paths.get(path).resolve(nombreFoto).toAbsolutePath();
+		
+		Resource imagen = new UrlResource(ruta.toUri());
+		
+		return Mono.just(
+				ResponseEntity.ok()
+				.header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + imagen.getFilename() + "\"")
+				.body(imagen)
+				);
 	}
 	
 	@GetMapping("/ver/{id}")
