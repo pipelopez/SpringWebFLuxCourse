503a36d21f31592e92f9bd5967b843358f3e8e2a
Pipe Lopez <jflopezv@sura.com.co>
2024-09-25 18:55:21 -0500

    Se adiciona función para guardar producto con foto usando form data

--- spring-boot-webflux-apirest/src/main/java/com/bolsadeideas/springboot/webflux/app/controllers/ProductoController.java
+++ spring-boot-webflux-apirest/src/main/java/com/bolsadeideas/springboot/webflux/app/controllers/ProductoController.java
@@ -37,6 +37,25 @@ public class ProductoController {
 	@Value("${config.uploads.path}")
 	private String path;
 	
+	@PostMapping("/v2")
+	public Mono<ResponseEntity<Producto>> crearConFoto(Producto producto, @RequestPart FilePart file) {
+		
+		if(producto.getCreateAt()==null) {
+			producto.setCreateAt(new Date());
+		}
+		
+		producto.setFoto(UUID.randomUUID().toString()+ "-" + file.filename()
+		.replace(" ", "")
+		.replace(":", "")
+		.replace("\\", ""));
+		
+		return file.transferTo(new File(path + producto.getFoto())).then(service.save(producto))
+				.map(p -> ResponseEntity
+				.created(URI.create("/api/productos/".concat(p.getId())))
+				.contentType(MediaType.APPLICATION_JSON_UTF8)
+				.body(p));
+	}
+	
 	@PostMapping("/upload/{id}")
 	public Mono<ResponseEntity<Producto>> upload(@PathVariable String id, @RequestPart FilePart file){
 		return service.findById(id).flatMap(p -> {
