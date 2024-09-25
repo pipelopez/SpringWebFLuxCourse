d32f5083e81c2bc2d070380c2eccc5e5d587f9b1
Pipe Lopez <jflopezv@sura.com.co>
2024-09-25 17:55:21 -0500

    Se adiciona la función de edición para el producto

--- spring-boot-webflux-apirest/src/main/java/com/bolsadeideas/springboot/webflux/app/controllers/ProductoController.java
+++ spring-boot-webflux-apirest/src/main/java/com/bolsadeideas/springboot/webflux/app/controllers/ProductoController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -53,5 +54,18 @@ public class ProductoController {
 				.created(URI.create("/api/productos/".concat(p.getId())))
 				.contentType(MediaType.APPLICATION_JSON_UTF8)
 				.body(p));
+	}
+	
+	@PutMapping("/{id}")
+	public Mono<ResponseEntity<Producto>> editar(@RequestBody Producto producto, @PathVariable String id) {
+		return service.findById(id).flatMap(p -> {
+			p.setNombre(producto.getNombre());
+			p.setPrecio(producto.getPrecio());
+			p.setCategoria(producto.getCategoria());
+			return service.save(p);
+		}).map(p -> ResponseEntity.created(URI.create("/api/productos/".concat(p.getId())))
+				.contentType(MediaType.APPLICATION_JSON_UTF8)
+				.body(p))
+				.defaultIfEmpty(ResponseEntity.notFound().build());			
 	}
 }
