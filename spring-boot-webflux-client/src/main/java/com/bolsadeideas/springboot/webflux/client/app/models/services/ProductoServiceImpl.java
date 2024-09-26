b4ca4335b14c8171ba0bc2216cb1b6b93139d7bf
Pipe Lopez <jflopezv@sura.com.co>
2024-09-26 16:45:47 -0500

    Se adicionan las funciones para los demás métodos del CRUD

--- spring-boot-webflux-client/src/main/java/com/bolsadeideas/springboot/webflux/client/app/models/services/ProductoServiceImpl.java
+++ spring-boot-webflux-client/src/main/java/com/bolsadeideas/springboot/webflux/client/app/models/services/ProductoServiceImpl.java
@@ -1,5 +1,76 @@
 package com.bolsadeideas.springboot.webflux.client.app.models.services;
 
-public class ProductoServiceImpl {
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import static org.springframework.http.MediaType.*;
+import org.springframework.stereotype.Service;
+import static org.springframework.web.reactive.function.BodyInserters.*;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import com.bolsadeideas.springboot.webflux.client.app.models.Producto;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Service
+public class ProductoServiceImpl implements ProductoService {
+
+	@Autowired
+	private WebClient client;
+	
+	@Override
+	public Flux<Producto> findAll() {
+		return client.get().accept(APPLICATION_JSON_UTF8)
+				.exchange()
+				.flatMapMany(response -> response.bodyToFlux(Producto.class));
+	}
+
+	@Override
+	public Mono<Producto> findById(String id) {
+		Map<String, Object> params = new HashMap<String, Object>();
+		params.put("id", id);
+		return client.get().uri("/{id}", params)
+				.accept(APPLICATION_JSON_UTF8)
+				.retrieve()
+				.bodyToMono(Producto.class);
+				//.exchange() //descomentar estas y comentar .retrieve y .bodyTomono funciona igual
+				//.flatMap(response -> response.bodyToMono(Producto.class));
+	}
+
+	@Override
+	public Mono<Producto> save(Producto producto) {
+		return client.post()
+				.accept(APPLICATION_JSON_UTF8)
+				.contentType(APPLICATION_JSON_UTF8)
+				//.body(fromObject(producto)) // descomentar esta funciona igual que el syncbody, solo dejar una de las dos
+				.syncBody(producto)
+				.retrieve()
+				.bodyToMono(Producto.class);
+	}
+
+	@Override
+	public Mono<Producto> update(Producto producto, String id) {
+
+		Map<String, Object> params = new HashMap<String, Object>();
+		params.put("id", id);
+		
+		return client.put()
+				.uri("/{id}", Collections.singletonMap("id", id))
+				.accept(APPLICATION_JSON_UTF8)
+				.contentType(APPLICATION_JSON_UTF8)
+				.syncBody(producto)
+				.retrieve()
+				.bodyToMono(Producto.class);
+	}
+
+	@Override
+	public Mono<Void> eliminar(String id) {
+		return client.delete().uri("/{id}", Collections.singletonMap("id", id))
+				.exchange()
+				.then();
+	}
 
 }
