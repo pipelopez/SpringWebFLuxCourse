2e926e28a8f9b0212624087b776ca5e9be8f607a
Pipe Lopez <jflopezv@sura.com.co>
2024-09-26 16:24:25 -0500

    Se crea el webclient

---
+++ spring-boot-webflux-client/src/main/java/com/bolsadeideas/springboot/webflux/client/app/AppConfig.java
@@ -0,0 +1,19 @@
+package com.bolsadeideas.springboot.webflux.client.app;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class AppConfig {
+	
+	@Value("${config.base.endpoint}")
+	private String url;
+	
+	@Bean
+	public WebClient registrarWebCLient() {
+		return WebClient.create(url);
+	}
+
+}
