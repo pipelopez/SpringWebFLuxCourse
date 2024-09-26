c85c27935efbc94f6855e7427d6fd5746d9d84f7
Pipe Lopez <jflopezv@sura.com.co>
2024-09-26 17:59:16 -0500

    Se optimiza el manejo de errores

--- spring-boot-webflux-client/src/main/java/com/bolsadeideas/springboot/webflux/client/app/handler/ProductoHandler.java
+++ spring-boot-webflux-client/src/main/java/com/bolsadeideas/springboot/webflux/client/app/handler/ProductoHandler.java
@@ -33,23 +33,11 @@ public class ProductoHandler {
 
 	public Mono<ServerResponse> ver(ServerRequest request) {
 		String id = request.pathVariable("id");
-		return service.findById(id)
+		return errorHandler(service.findById(id)
 				.flatMap(p -> ServerResponse.ok()
 						.contentType(APPLICATION_JSON_UTF8)
 						.syncBody(p))
-				.switchIfEmpty(ServerResponse.notFound().build())
-				.onErrorResume(error -> {
-					WebClientResponseException errorResponse = (WebClientResponseException) error;
-					if(errorResponse.getStatusCode() == HttpStatus.NOT_FOUND) {
-						Map<String, Object> body = new HashMap<>();
-						body.put("error", "No existe el producto: ".concat(errorResponse.getMessage()));
-						body.put("timestamp", new Date());
-						body.put("status", errorResponse.getStatusCode().value());
-						return ServerResponse.status(HttpStatus.NOT_FOUND).syncBody(body);
-						
-					}
-					return Mono.error(errorResponse);
-				});
+				.switchIfEmpty(ServerResponse.notFound().build()));
 
 	}
 	
@@ -80,53 +68,45 @@ public class ProductoHandler {
 		Mono<Producto> producto = request.bodyToMono(Producto.class);
 		String id = request.pathVariable("id");
 		
-		return producto
+		return errorHandler(producto
 				.flatMap(p -> service.update(p, id))
 				.flatMap(p -> ServerResponse.created(URI.create("/api/client".concat(p.getId())))
 				.contentType(APPLICATION_JSON_UTF8)
-				.syncBody(p))
-				.onErrorResume(error -> {
-					WebClientResponseException errorResponse = (WebClientResponseException) error;
-					if(errorResponse.getStatusCode() == HttpStatus.NOT_FOUND) {
-						return ServerResponse.notFound().build();
-						
-					}
-					return Mono.error(errorResponse);
-				});
+				.syncBody(p)));
 	}
 	
 
 	
 	public Mono<ServerResponse> eliminar(ServerRequest request){
 		String id = request.pathVariable("id");		
-		return service.delete(id).then(ServerResponse.noContent().build())
-				.onErrorResume(error -> {
-					WebClientResponseException errorResponse = (WebClientResponseException) error;
-					if(errorResponse.getStatusCode() == HttpStatus.NOT_FOUND) {
-						return ServerResponse.notFound().build();
-						
-					}
-					return Mono.error(errorResponse);
-				});
+		return errorHandler(service.delete(id).then(ServerResponse.noContent().build()));
 	}
 	
 	public Mono<ServerResponse> upload(ServerRequest request){
 		String id = request.pathVariable("id");
 		
-		return request.multipartData().map(multipart -> multipart.toSingleValueMap().get("file"))
+		return errorHandler(request.multipartData().map(multipart -> multipart.toSingleValueMap().get("file"))
 				.cast(FilePart.class)
 				.flatMap(file -> service.upload(file, id))
 				.flatMap(p -> ServerResponse.created(URI.create("/api/client".concat(p.getId())))
 						.contentType(APPLICATION_JSON_UTF8)
 						.syncBody(p))
-				.onErrorResume(error -> {
-					WebClientResponseException errorResponse = (WebClientResponseException) error;
-					if(errorResponse.getStatusCode() == HttpStatus.NOT_FOUND) {
-						return ServerResponse.notFound().build();
-						
-					}
-					return Mono.error(errorResponse);
-				});		
+				);		
+	}
+	
+	private Mono<ServerResponse> errorHandler(Mono<ServerResponse> response){
+		return response.onErrorResume(error -> {
+			WebClientResponseException errorResponse = (WebClientResponseException) error;
+			if(errorResponse.getStatusCode() == HttpStatus.NOT_FOUND) {
+				Map<String, Object> body = new HashMap<>();
+				body.put("error", "No existe el producto: ".concat(errorResponse.getMessage()));
+				body.put("timestamp", new Date());
+				body.put("status", errorResponse.getStatusCode().value());
+				return ServerResponse.status(HttpStatus.NOT_FOUND).syncBody(body);
+				
+			}
+			return Mono.error(errorResponse);
+		});
 	}
 
 }
