983662ff40f26f3afa716e72d3e3365db1afbf6d
Pipe Lopez <jflopezv@sura.com.co>
2024-09-25 19:12:32 -0500

    Se mejora la validación de campos y mensajes de respuesta en la creación del producto.

--- spring-boot-webflux-apirest/src/main/java/com/bolsadeideas/springboot/webflux/app/controllers/ProductoController.java
+++ spring-boot-webflux-apirest/src/main/java/com/bolsadeideas/springboot/webflux/app/controllers/ProductoController.java
@@ -3,6 +3,8 @@ package com.bolsadeideas.springboot.webflux.app.controllers;
 import java.io.File;
 import java.net.URI;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,10 +22,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.support.WebExchangeBindException;
 
 import com.bolsadeideas.springboot.webflux.app.models.documents.Producto;
 import com.bolsadeideas.springboot.webflux.app.models.services.ProductoService;
 
+import jakarta.validation.Valid;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -86,16 +90,39 @@ public class ProductoController {
 	}
 	
 	@PostMapping
-	public Mono<ResponseEntity<Producto>> crear(@RequestBody Producto producto) {
+	public Mono<ResponseEntity<Map<String, Object>>> crear(@Valid @RequestBody Mono<Producto> monoProducto) {
 		
-		if(producto.getCreateAt()==null) {
-			producto.setCreateAt(new Date());
-		}
+		Map<String, Object> respuesta = new HashMap<String, Object>();
 		
-		return service.save(producto).map(p -> ResponseEntity
-				.created(URI.create("/api/productos/".concat(p.getId())))
-				.contentType(MediaType.APPLICATION_JSON_UTF8)
-				.body(p));
+		return monoProducto.flatMap(producto -> {
+			if(producto.getCreateAt()==null) {
+				producto.setCreateAt(new Date());
+			}
+			
+			return service.save(producto).map(p -> {
+				respuesta.put("producto", p);
+				respuesta.put("mensaje", "Producto creado con éxito");
+				respuesta.put("timestamp", new Date());
+				return ResponseEntity			
+					.created(URI.create("/api/productos/".concat(p.getId())))
+					.contentType(MediaType.APPLICATION_JSON_UTF8)
+					.body(respuesta);
+			});
+			
+		}).onErrorResume(t -> {
+			return Mono.just(t).cast(WebExchangeBindException.class)
+					.flatMap(e -> Mono.just(e.getFieldErrors()))
+					.flatMapMany(Flux::fromIterable)
+					.map(fieldError -> "El campo " + fieldError.getField() + " " + fieldError.getDefaultMessage())
+					.collectList()
+					.flatMap(list -> {
+						respuesta.put("errors", list);
+						respuesta.put("timestamp", new Date());
+						respuesta.put("status", HttpStatus.BAD_REQUEST.value());
+						return Mono.just(ResponseEntity.badRequest().body(respuesta));
+					});
+		});
+		
 	}
 	
 	@PutMapping("/{id}")
