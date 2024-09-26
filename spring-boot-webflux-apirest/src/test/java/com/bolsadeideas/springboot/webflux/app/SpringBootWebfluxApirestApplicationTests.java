562af5150259121a6634942124c5676c73f3f301
Pipe Lopez <jflopezv@sura.com.co>
2024-09-26 15:07:30 -0500

    Se hacen las primeras pruebas unitarias al listar.

--- spring-boot-webflux-apirest/src/test/java/com/bolsadeideas/springboot/webflux/app/SpringBootWebfluxApirestApplicationTests.java
+++ spring-boot-webflux-apirest/src/test/java/com/bolsadeideas/springboot/webflux/app/SpringBootWebfluxApirestApplicationTests.java
@@ -1,13 +1,33 @@
 package com.bolsadeideas.springboot.webflux.app;
 
+import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8;
+
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+import com.bolsadeideas.springboot.webflux.app.models.documents.Producto;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SpringBootWebfluxApirestApplicationTests {
-
+	
+	@Autowired
+	private WebTestClient client;
+	
 	@Test
-	void contextLoads() {
+	public void listarTest() {
+		client.get()
+		.uri("/api/v2/productos")
+		.accept(APPLICATION_JSON_UTF8)
+		.exchange()
+		.expectStatus().isOk()
+		.expectHeader().contentType(APPLICATION_JSON_UTF8)
+		.expectBodyList(Producto.class)
+		.hasSize(10);
 	}
 
 }
